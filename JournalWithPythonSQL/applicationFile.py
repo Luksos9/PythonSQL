01ac2786ad312464049c22c9f97339a4cc279a10
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-08-21 22:09:20 +0200

    Separating parts of our application dealing with getting user input from parts that store data. Also editing README.md

--- JournalWithPythonSQL/applicationFile.py
+++ JournalWithPythonSQL/applicationFile.py
@@ -1,3 +1,5 @@
+from dataBase import add_entry, get_entries
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -11,8 +13,15 @@ print(welcome)
 
 while (user_input := input(menu)) != "3":  # That is new Python 3.8 feature, so i dont have to put user_input
     if user_input == '1':                  # before and after the loop: user_input = input(menu)
-        print("Adding...")
+        entry_content = input("What have you learned today?")
+        entry_date = input("Enter the date: ")
+
+        add_entry(entry_content, entry_date)
     elif user_input == '2':
-        print('Viewing...')
+        entries = get_entries()
+
+        for entry in entries:
+            print(f"{entry['date']}\n{entry['content']}\n\n")
+
     else:
         print('Invalid option, please try again!')
--- JournalWithPythonSQL/dataBase.py
+++ JournalWithPythonSQL/dataBase.py
@@ -1,14 +1,12 @@
 entries = []
 
 
-def add_entry():
+def add_entry(entry_content, entry_date):
     entry_content = input("What have you learned today?")
     entry_date = input("Enter the date: ")
 
     entries.append({"content": entry_content, "date": entry_date})
 
-def view_entries():
-    """"""
-    for entry in entries:
-        print(f"{entry['date']}\n{entry['content']}\n\n")
+def get_entries():
+    return entries
 
