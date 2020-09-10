3c7828535fb62905965325aeb07a7a329951ecf9
Łukasz Szumilas <lukaszszumilas9@wp.pl>
2020-09-10 14:48:04 +0200

    Added module that nicely displays charts from out poll database and many adjustments to it

--- PollApplication/chartHandling/charts.py
+++ PollApplication/chartHandling/charts.py
@@ -12,3 +12,20 @@ def create_pie_chart(options):
     )
 
     return figure
+
+
+def create_bar_chart(polls):
+    figure = plt.figure(figsize=(10, 10))
+    figure.subplots_adjust(bottom=0.35)  # gives more room underneath (works without it too)
+    axes = figure.add_subplot(1, 1, 1)
+    axes.set_title("Polls and their total votes")
+    axes.set_ylabel("Vote count")
+
+    axes.bar(
+        range(len(polls)),  # how wide (x coordinates)
+        [poll[1] for poll in polls],  # how many % of total voices were for this poll (height)
+        tick_label=[poll[0] for poll in polls]  # labels for each bar
+    )
+    plt.xticks(rotation=30, ha="center")  # adds rotation, and center alignment to labels
+
+    return figure
--- PollApplication/chartHandling/database.py
+++ PollApplication/chartHandling/database.py
@@ -14,8 +14,8 @@ GROUP BY polls.title;"""
 connection = psycopg2.connect(os.environ.get("DATABASE_URI"))
 
 
-def get_options(poll_id: int):
+def get_polls_and_votes():
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(SELECT_POLLS_AND_VOTES, (poll_id,))
+            cursor.execute(SELECT_POLLS_AND_VOTES)
             return cursor.fetchall()
